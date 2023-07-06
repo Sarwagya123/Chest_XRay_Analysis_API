f38a217c980dcbe0ca52199418bdf0387c9a13a7
Sarwagya123 <sarwagyamxn17@gmail.com>
2023-07-06 21:29:15 +0530

    Changes in some files

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask, request, jsonify
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
+# from PIL import Image
 
-model = pickle.load(open('better model.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
 app=Flask(__name__)
 
@@ -16,7 +17,10 @@ def img_to_array(imagefile):
     data=[]
     img = plt.imread(imagefile)
     img = cv2.resize(img, (150, 150))
-    img = np.dstack([img, img, img])
+    img = np.reshape(img,[150,150,3])
+    # image = Image.fromarray(img)
+    # image.save("Image.jpg")
+    # img = np.dstack([img, img, img])
     img = img.astype('float32') / 255
     data.append(img)
     return(np.array(data))
@@ -29,6 +33,11 @@ def predict():
     input_query = img_to_array(xray_image)
 
     result = model.predict(input_query)
+    # ans=""
+    # if(result>0.7):
+    #     ans="Pneumonia"
+    # else:
+    #     ans="Not Pneumonia"
 
     return jsonify({'result':str(result)})
 
